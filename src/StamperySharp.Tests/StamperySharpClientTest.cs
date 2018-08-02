5d1d6d4e47325a583363151a0411adc9c3003299
Matias Dieguez <matiasdieguez@hotmail.com>
2018-08-02 17:32:17 -0300

    Fix versión inicial

--- src/StamperySharp.Tests/StamperySharpClientTest.cs
+++ src/StamperySharp.Tests/StamperySharpClientTest.cs
@@ -9,7 +9,7 @@ namespace StamperySharp.Tests
         [TestMethod]
         public void StampTest()
         {
-            var stamperyClient = new StamperySharpClient("d3e5a67964d6be0", "254b810c-6292-43b1-c5e5-59cda63bbf1f");
+            var stamperyClient = new StamperySharpClient("your-client-id", "your-secret");
             var result = stamperyClient.Stamp("fa814e37ad092518b0b33ed1f21c8bac4daed663435abba01e369399522279e5");
             Assert.IsNotNull(result);
         }
